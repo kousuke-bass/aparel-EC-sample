d7179f41b1a72e7ea0fccc0f400f31b44fbaed40
kousuke-bass <70252775+kousuke-bass@users.noreply.github.com>
2020-09-15 20:40:16 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(function(){
 		}
 		items.forEach(function(item, index){
 			html_template += `<li class="item" data-item-id="${item['id']}">
-			    <a href="detail.html?id=${item['id']}">
+			    <a href="../main-html/index.html?id=${item['id']}">
 			      <div class="item-cap"><img src="item/${item['id']}.png" loading="lazy"></div>
 			      <div class="item-info">
 			        <h3 class="item-name">${item['name']}</h3>
